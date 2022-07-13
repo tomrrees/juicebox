1bdf041d3807a2b6e50325bcd3fdcf4b01193946
nicofrancis <nicolofrancis10@gmail.com>
2022-07-13 15:24:02 -0600

    day 2 complete

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,17 @@
 // inside db/seed.js
 
 // grab our client with destructuring from the export in index.js
-const { client, getAllUsers, createUser } = require("./index");
+const {
+  client,
+  getAllUsers,
+  createUser,
+  updateUser,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser,
+  getUserById,
+} = require("./index");
 
 async function dropTables() {
   try {
@@ -9,8 +19,9 @@ async function dropTables() {
 
     await client.query(`
 
+      DROP TABLE IF EXISTS posts;
       DROP TABLE IF EXISTS users;
-  
+      
       `);
 
     console.log("Finished dropping tables!");
@@ -30,11 +41,28 @@ async function createTables() {
     CREATE TABLE users(
         id SERIAL PRIMARY KEY,
         username VARCHAR (255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        password varchar(255) NOT NULL,
+        name VARCHAR (255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true
+     );
+  
+      `);
+    console.log("Starting to build POSTS table...");
+
+    await client.query(`
+
+    CREATE TABLE posts(
+      id SERIAL PRIMARY KEY,
+      "authorId" INTEGER REFERENCES users(id) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      content TEXT NOT NULL,
+      active BOOLEAN DEFAULT true 
      );
   
       `);
 
+    console.log("Finished building POSTS tables!");
     console.log("Finished building tables!");
   } catch (error) {
     console.error("Error building tables!");
@@ -50,15 +78,21 @@ async function createInitialUsers() {
     const albert = await createUser({
       username: "albert",
       password: "bertie99",
+      name: "albert",
+      location: "denver",
     });
 
     const sandra = await createUser({
       username: "sandra",
       password: "2sandy4me",
+      name: "sandra",
+      location: "california",
     });
     const glamgal = await createUser({
       username: "glamgal",
       password: "soglam",
+      name: "glamgal",
+      location: "utah",
     });
 
     console.log(albert);
@@ -70,13 +104,35 @@ async function createInitialUsers() {
   }
 }
 
-async function rebuildDB() {
+async function createInitialPosts() {
   try {
-    client.connect();
+    const [albert, sandra, glamgal] = await getAllUsers();
 
+    console.log("Starting to create posts");
+
+    await createPost({
+      authorId: albert.id,
+      title: "First Post",
+      content:
+        "This is my first post. I hope I love writing blogs as much as I love writing them.",
+    });
+
+    // create posts for sandra glamgal
+
+    console.log("Finsihed creating posts!");
+    // a couple more
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  client.connect();
+  try {
     await dropTables();
     await createTables();
     await createInitialUsers();
+    await createInitialPosts();
   } catch (error) {
     console.error(error);
   }
@@ -86,8 +142,33 @@ async function testDB() {
   try {
     console.log("Starting to test database...");
 
+    console.log("Calling getAllUsers");
+
     const users = await getAllUsers();
     console.log("getAllUsers:", users);
+    console.log("Result:", users);
+
+    console.log("Calling updateUser on users[0]");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY",
+    });
+    console.log("Result:", updateUserResult);
+
+    console.log("Calling getAllPosts");
+    const posts = await getAllPosts();
+    console.log("Result:", posts);
+
+    console.log("Calling updatePost on posts[0]");
+    const updatePostResult = await updatePost(posts[0].id, {
+      title: "New Title",
+      content: "Updated Content",
+    });
+    console.log("Result:", updatePostResult);
+
+    console.log("Calling getUserById with 1");
+    const albert = await getUserById(1);
+    console.log("Result:", albert);
 
     console.log("Finished database tests!");
   } catch (error) {
@@ -99,4 +180,7 @@ async function testDB() {
 rebuildDB()
   .then(testDB)
   .catch(console.error)
-  .finally(() => client.end());
+  .finally(() => {
+    console.log("about to end the client");
+    client.end();
+  });
